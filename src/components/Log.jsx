9faa06f272775844939dd172dbcfd3c01fa2c282
ImanDesign <imandesign@yahoo.com>
2024-01-11 18:58:35 +0330

    Final commit

---
+++ src/components/Log.jsx
@@ -0,0 +1,7 @@
+export default function Log({turns}) {
+  return <ol id="log">
+    {turns.map((turn, index) => (
+      <li key={index}>{turn.player} select {turn.square.row},{turn.square.col}</li>
+    ))}
+  </ol>;
+}
---
+++ src/components/Player.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol, isActive, onNameChange }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing); // Best practice updating State based on old State correctly because React schedules States update
+
+    if(isEditing) {
+      onNameChange(symbol, playerName);
+    }
+  }
+
+  function handleInputChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li className={isActive ? 'active' : undefined}>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input
+            type="text"
+            value={playerName}
+            onChange={handleInputChange}
+            required
+            className="player-name"
+          />
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{!isEditing ? "Edit" : "Save"}</button>
+    </li>
+  );
+}
